733d9a2e24a3cd33d2b5aed4efe0437be03be404
Jorge Yau <jyau01@citymail.cuny.edu>
2015-03-08 13:31:24 -0400

    Create gulpfile.js

---
+++ collection/library/gulpfile.js
@@ -0,0 +1,38 @@
+/*!
+ * gulpfile.js
+ * MIT License (c) 2015
+ */
+'use strict';
+
+// Load dependecies
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var rimraf = require('rimraf');
+var minifyCSS = require('gulp-minify-css');
+var strReplace = require('gulp-replace');
+
+// [Task] removes 'build/'
+gulp.task('clean', function(cb) {
+  rimraf('build/', cb);
+});
+
+// [Task] uglify js
+gulp.task('uglify', function() {
+  return gulp.src('src/*.js')
+    .pipe(strReplace(/'use strict';/g, ''))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('build'));
+});
+
+// [Task] minify css
+gulp.task('minify', function() {
+  return gulp.src('src/*.css')
+    .pipe(minifyCSS())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('build'));
+});
+
+// [Task] generates build
+gulp.task('default', ['uglify', 'minify']);
