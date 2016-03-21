9e0fc602dc7da0b84c198bd91ee11366b760cb66
Jorge Yau <codenameyau@users.noreply.github.com>
2016-03-21 12:48:13 -0400

    Update and rename node-server-gulpfile.js to node-inspector-gulpfile.js

---
+++ collection/backend/node-inspector-gulpfile.js
@@ -0,0 +1,29 @@
+/*!
+ * node-inspector-gulpfile.js
+ * Visit 'http://localhost:5000?port=5858' to listen to the debugger.
+ */
+'use strict';
+
+var gulp = require('gulp')
+var nodemon = require('gulp-nodemon');
+
+var PROJECT_DIR = path.resolve(__dirname);
+var APP_DIR = path.resolve(PROJECT_DIR, 'app');
+
+
+gulp.task('debug', function() {
+  return nodemon({
+    execMap: {
+      js: 'node-inspector --web-port=5000 & nodemon --debug'
+    },
+    ext: 'js',
+    ignore: ['node_modules/*'],
+    cwd: APP_DIR,
+    verbose: true,
+    script: 'server.js',
+
+    // Optional. The following two are unused.
+    // env: { 'NODE_ENV': 'development' },
+    // args: ['local'],
+  });
+});
